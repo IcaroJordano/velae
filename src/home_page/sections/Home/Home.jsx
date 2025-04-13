d32a4363f1e163ecab112be810083434c7db483e
Icaro Jordano <Icarojordano>
2025-04-13 00:35:45 -0300

    ajustando imagem de home

--- src/home_page/sections/Home/Home.jsx
+++ src/home_page/sections/Home/Home.jsx
@@ -1,9 +1,10 @@
-
+import image from '../../../images/bg_home2.jpg'
 const Home = () => {
     
     return (
         <section className="pt-3  lg:pt-10 lg:h-screen lg:overflow-hidden  h-7/12   w-full ">
-            <img className="absolute w-full overflow-hidden h-8/12 object-cover  lg:object-top   lg:h-11/12  "  src="src/images/bg_home.jpg" alt="" />
+            <img src="" alt="" />
+            <img className="absolute w-full overflow-hidden h-8/12 object-cover  lg:object-top   lg:h-11/12  "  src={image} alt="" />
             <div className="mx-auto text-center  text-neutral-50  w-3/4 left-1/2 -translate-x-1/2 h-36  top-7/12 lg:top-9/12 absolute  ">
                 <h2 className="font-extralight tracking-widest text-sm">Lancamento</h2>
                 <h3 className="text-3xl my-3 ">Colecao Feminina Noutical</h3>
